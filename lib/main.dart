ab96610cabf76f142a17a4ae211c582feada0d9f
mckeownsena-droid <mckeownsena@gmail.com>
2026-08-08 11:14:00 +0100

    Use clean profitability MVP v4

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'src/app_v3.dart';
+import 'src/app_v4.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
