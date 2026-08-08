bdc28ce2f0438084da96ac4c8439c210b4f49022
mckeownsena-droid <mckeownsena@gmail.com>
2026-08-08 08:48:32 +0100

    Add Firebase web initialization for FarmersHub testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'src/app_v2.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAbxq0GIyW30edgLnNLiV6uXNKjSzeApJQ',
+        authDomain: 'farmershub-gh-new.firebaseapp.com',
+        projectId: 'farmershub-gh-new',
+        storageBucket: 'farmershub-gh-new.firebasestorage.app',
+        messagingSenderId: '379868120125',
+        appId: '1:379868120125:android:2c6919c06deebda3be1654',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const FarmersHubApp());
 }
