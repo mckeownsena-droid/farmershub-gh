f5ac9bd35088be4543e52d0cef55169206d8e7c2
mckeownsena-droid <mckeownsena@gmail.com>
2026-08-08 08:10:31 +0100

    Switch app entrypoint to expanded MVP

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'src/app.dart';
+import 'src/app_v2.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
