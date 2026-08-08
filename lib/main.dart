dcc27ec1ede6f47d77b124c4a9726b40d211fdb2
mckeownsena-droid <mckeownsena@gmail.com>
2026-08-08 13:01:18 +0100

    Enable explicit Firestore offline persistence on mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,10 @@ Future<void> main() async {
     );
   } else {
     await Firebase.initializeApp();
+    FirebaseFirestore.instance.settings = const Settings(
+      persistenceEnabled: true,
+      cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
+    );
   }
 
   runApp(const FarmersHubAppV5());
